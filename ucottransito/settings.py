5339d75e1006cbd91b3a475642bdeda7422c3a8a
Adrianferma <152186883+Adrianferma@users.noreply.github.com>
2024-07-25 12:35:40 -0500

    Update settings.py

--- ucottransito/settings.py
+++ ucottransito/settings.py
@@ -124,19 +124,24 @@ USE_L10N = True
 
 USE_TZ = True
 
+
+
 IMPORT_EXPORT_USE_TRANSACTIONS = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-    
+    os.path.join(BASE_DIR, 'static'),  # This is correct for development
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
+# STATIC_ROOT is typically set to a different directory for collected static files
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_collected')
 STATIC_URL = '/static/'
 
+# Media files (uploads)
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 
