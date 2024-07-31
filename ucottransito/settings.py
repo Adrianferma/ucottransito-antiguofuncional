dbfd2dddaf7787976f84d8c7075f9b03db412a3f
Adrianferma <152186883+Adrianferma@users.noreply.github.com>
2024-07-31 16:11:18 -0500

    Update settings.py

--- ucottransito/settings.py
+++ ucottransito/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'p=u*62l=1!w#cf-nzo^2*l^e+43_%i)^1xe44(m@04$syrt$ge'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['18.117.245.254', '127.0.0.1']
+ALLOWED_HOSTS = ['18.117.245.254', 'www.ucotcitacione.com', 'ucotcitacione.com']
 
 # Application definition
 
