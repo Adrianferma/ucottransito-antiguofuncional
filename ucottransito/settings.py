cea70c83162086778de972260da66b8b94f2b215
Adrianferma <152186883+Adrianferma@users.noreply.github.com>
2024-08-04 10:56:08 -0500

    Update settings.py

--- ucottransito/settings.py
+++ ucottransito/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'p=u*62l=1!w#cf-nzo^2*l^e+43_%i)^1xe44(m@04$syrt$ge'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['www.ucotcitaciones.com', 'ucotcitaciones.com', '18.227.46.157']
+ALLOWED_HOSTS = ['www.ucotcitaciones.com', 'ucotcitaciones.com', '18.188.131.156']
 
 # Application definition
 
@@ -85,7 +85,7 @@ DATABASES = {
         'NAME': 'newdb',
         'USER': 'newuser',
         'PASSWORD': 'newpass',
-        'HOST': '18.227.46.157',
+        'HOST': '18.188.131.156',
         'PORT': 5432,
         'OPTIONS': {
             'options': '-c timezone=UTC',
